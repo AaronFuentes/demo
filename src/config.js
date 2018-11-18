7e5011e0b48c3b83c0b7621fe606d402e3bc0225
Rodrigo Martínez <dev@brunneis.com>
2018-11-18 21:28:15 +0100

    Update config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const LOCATION_INTERVAL = 5000;
-export const API_URL = 'http://alastria-open-call-demo.councilbox.brunneis.com:8080';
-export const CLIENT_URL = 'http://alastria-open-call-demo.councilbox.brunneis.com';
+export const API_URL = 'https://api.alastria-open-call-demo.councilbox.brunneis.com';
+export const CLIENT_URL = 'https://alastria-open-call-demo.councilbox.brunneis.com';
