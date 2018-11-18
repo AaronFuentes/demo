0b1db9a674b5d7a5ba78b17bf536683af13627d4
Aaron Fuentes <aaron.fuentes@cocodin.com>
2018-11-18 22:39:39 +0100

    Trim hash values

--- src/utils/hashUtils.js
+++ src/utils/hashUtils.js
@@ -1,9 +1,9 @@
 export const extractHashFromURL = url => {
     const index = url.indexOf('tracking/');
     if(index === -1){
-        return url.replace(/"/g, '').replace(/'/g, '');
+        return url.replace(/"/g, '').replace(/'/g, '').trim();
     }
     let hash = url.substr(index + 9);
-    hash.replace(/"/g, '').replace(/'/g, '');;
+    hash.replace(/"/g, '').replace(/'/g, '').trim();
     return hash;
 }
